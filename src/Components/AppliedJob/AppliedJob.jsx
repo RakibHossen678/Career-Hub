0c59d8ed59064e0c1b94c2d7c735f2dd110bca84
RakibHossen678 <rh491464@gmail.com>
2024-03-23 20:53:18 +0600

    added functionaltty on the filter

--- src/Components/AppliedJob/AppliedJob.jsx
+++ src/Components/AppliedJob/AppliedJob.jsx
@@ -1,9 +1,43 @@
-const AppliedJob = () => {
-    return (
+import { Link } from "react-router-dom";
+
+const AppliedJob = ({ appliedJob }) => {
+  const {
+    id,
+    logo,
+    job_title,
+    salary,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+  } = appliedJob;
+  // console.log(appliedJob);
+  return (
+    <div className="my-4 py-4 px-3 flex justify-between items-center border-2 ">
+      <div className="flex items-center space-x-4">
         <div>
-            
+          <img className="w-44" src={logo} alt="" />
         </div>
-    );
+        <div>
+          <h1 className="font-semibold">{job_title}</h1>
+          <p className="py-1">{company_name}</p>
+          <span className="flex space-x-2 my-2">
+            <p className="border-2 px-2 rounded-lg">{remote_or_onsite}</p>
+            <p className="border-2 px-2 rounded-lg m">{job_type}</p>
+          </span>
+          <p>
+            <span>{location}</span>
+            <span>{salary}</span>
+          </p>
+        </div>
+      </div>
+      <Link to={`job/${id}`}>
+        <button className="flex justify-start bg-gradient-to-r from-[#7E90FE] to-[#9873FF] w-32 px-3 py-3 items-center text-white font-bold rounded-lg">
+          View Details
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default AppliedJob;
--- src/Components/Appliedjobs/AppliedJobs.jsx
+++ src/Components/Appliedjobs/AppliedJobs.jsx
@@ -1,8 +1,28 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../Utility/LocalStorage";
+import AppliedJob from "../AppliedJob/AppliedJob";
 const AppliedJobs = () => {
   const [appliedJobs, setAppliedJObs] = useState([]);
+  const [display,setDisplay]=useState([]);
+  // console.log(display)
+  const handleJObsFilter=Filter=>{
+    if(Filter==='All'){
+      setDisplay(appliedJobs)
+    }
+    else if(Filter==='remote'){
+      const remoteJobs=appliedJobs.filter(job=>job.remote_or_onsite ==='Remote')
+      // console.log(remoteJobs)
+      setDisplay(remoteJobs)
+      // console.log(display)
+    }
+    else if (Filter==='onsite'){
+      const onsiteJObs=appliedJobs.filter(job=>job.remote_or_onsite==='Onsite')
+      // console.log(onsiteJObs)
+      setDisplay(onsiteJObs)
+      console.log(display)
+    }
+  }
   const Jobs = useLoaderData();
   // console.log(Jobs)
   useEffect(() => {
@@ -13,16 +33,38 @@ const AppliedJobs = () => {
       // const jobsApplied=[];
       // console.log(jobsApplied)s
       setAppliedJObs(jobsApplied);
+      setDisplay(jobsApplied)
     }
   }, []);
-  // console.log(appliedJobs)
+  // console.log(display);
   return (
-    <div>
+    <div >
       <div className="bg-gradient-to-br from-[#e5e8fb] to-[#f1f0f4] h-44 text-2xl font-semibold flex justify-center items-center">
         <h2>Applied Jobs</h2>
       </div>
-      <div className="w-9/12 max-auto">
-        <div></div>
+      <div className="dropdown flex justify-end w-10/12 pt-7  py-20">
+        <div tabIndex={0} role="button" className="btn m-1 text-lg">
+          Filter
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-44"
+        >
+          <li onClick={()=>handleJObsFilter('All')}>
+            <a>All</a>
+          </li>
+          <li onClick={()=>handleJObsFilter('remote')}>
+            <a>Remote</a>
+          </li>
+          <li onClick={()=>handleJObsFilter('onsite')}>
+            <a>Onsite</a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-9/12 mx-auto ">
+        {display.map((appliedJob) => (
+          <AppliedJob key={appliedJob.id} appliedJob={appliedJob}></AppliedJob>
+        ))}
       </div>
     </div>
   );
